ffec2251814abc75d8753926e70adb20801ec1b9
Paulo Felipe <paulo.felipe.pfsv@gmail.com>
2025-06-19 16:39:36 -0300

    Adição dos componentes no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
+import { Container } from './components/Container/';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+
 import './styles/theme.css';
 import './styles/global.css';
-import { Heading } from './components/Heading/';
-import { Container } from './components/Container/';
+
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>Welcome to Ignite Lab</Heading>
+        <Logo/>
+      </Container>
+
+      <Container>
+        <Menu>
+        </Menu>
       </Container>
     </>
   )
