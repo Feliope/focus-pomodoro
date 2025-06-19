68d7999046d0b6e332f66bcfc2651486a0df93f5
Paulo Felipe <paulo.felipe.pfsv@gmail.com>
2025-06-19 16:49:30 -0300

    Alteração do Icon da Logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,11 +1,11 @@
-import { TimerIcon } from 'lucide-react';
+import { HourglassIcon } from 'lucide-react';
 import styles from './styles.module.css';
 
 export function Logo() {
   return (
     <div className={styles.logo}>
       <a className={ styles.logoLink } href="#">
-        <TimerIcon/>
+        <HourglassIcon/>
         <span>Focus</span>
       </a>
     </div>
