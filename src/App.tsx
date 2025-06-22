a81094a28096fb83c53fae605b5fa2207fcc329a
Paulo Felipe <paulo.felipe.pfsv@gmail.com>
2025-06-21 23:18:25 -0300

    Adição do componente input

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,48 @@ import { Menu } from './components/Menu';
 import './styles/theme.css';
 import './styles/global.css';
 import { CountDown } from './components/CountDown';
+import { Input } from './components/Input';
 
 
 export function App() {
   return (
     <>
       <Container>
-        <Logo/>
+        <Logo />
       </Container>
 
       <Container>
-        <Menu/>
+        <Menu />
       </Container>
 
       <Container>
-        <CountDown></CountDown>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className="form" action="">
+          <div className="formRow">
+            <Input
+              id='input'
+              type='text'
+              labelText='Nome do ciclo'
+              placeholder='Dê um nome para o ciclo'
+            />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0</p>
+          </div>
+
+          <div className="formRow">
+            <button>enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   )
