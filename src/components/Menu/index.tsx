09fd49eb354a2d104352ce10491b72025231ebec
Paulo Felipe <paulo.felipe.pfsv@gmail.com>
2025-06-24 18:57:06 -0300

    Implementação da alteração de temas utilizando useState e useEffect

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,23 +1,59 @@
-import { BoltIcon, HistoryIcon, HouseIcon, SunIcon } from 'lucide-react';
+import { BoltIcon, HistoryIcon, HouseIcon, MoonIcon, SunIcon } from 'lucide-react';
+import { useState, useEffect } from 'react';
 import styles from './styles.module.css';
 
+type AvailableThemes = 'dark' | 'light';
+
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme = localStorage.getItem('theme') as AvailableThemes || 'dark';
+    return storageTheme;
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />
+  }
+
+  function handleThemeChange(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    event.preventDefault();
+    setTheme(prevTheme => prevTheme === 'dark' ? 'light' : 'dark');
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
   return (
     <nav className={styles.menu}>
-      <a className={ styles.menuLink } href="#">
-        <HouseIcon/>
+      <a className={styles.menuLink} href="#"
+        aria-label='Ir para a Home'
+        title='Home'
+      >
+        <HouseIcon />
       </a>
 
-      <a className={ styles.menuLink } href="#">
-        <HistoryIcon/>
+      <a className={styles.menuLink} href="#"
+        aria-label='Ver Histórico'
+        title='Histórico'
+      >
+        <HistoryIcon />
       </a>
 
-      <a className={ styles.menuLink } href="#">
-        <BoltIcon/>
+      <a className={styles.menuLink} href="#"
+        aria-label='Ir para as Configurações'
+        title='Configurações'
+      >
+        <BoltIcon />
       </a>
 
-      <a className={ styles.menuLink } href="#">
-        <SunIcon/>
+      <a className={styles.menuLink} href="#"
+        aria-label='Mudar Tema'
+        title='Tema'
+        onClick={event => handleThemeChange(event)}
+      >
+        { nextThemeIcon[theme] }
       </a>
     </nav>
   )
