aeace7cc528ea8230be354afe6765889f1863134
Paulo Felipe <paulo.felipe.pfsv@gmail.com>
2025-09-18 18:31:01 -0300

    Lógica do formulário

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,3 +1,8 @@
+import { useRef } from 'react';
+import type { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { getNextCycle } from '../../utils/getNextCycle';
+
 import { Input } from '../Input';
 import { Cycles } from '../Cycles';
 import { Button } from '../Button';
@@ -5,17 +10,61 @@ import { Button } from '../Button';
 import { PlayCircleIcon } from 'lucide-react';
 
 import styles from './styles.module.css';
-
+import { getNextCycleType } from '../../utils/getNextCycleType';
 
 export function MainForm() {
+  const { state, setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  const nextCycle = getNextCycle(state.currentCycle);
+  const nextCycleType = getNextCycleType(nextCycle);
+
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert('Digite o nome da tarefa')
+      return
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: state.config[ nextCycleType ],
+      type: nextCycleType
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining,
+        formattedSecondsRemaining: '00:00',
+        tasks: [...prevState.tasks, newTask],
+      }
+    })
+  }
+
   return (
-    <form className={styles.form} action="">
+    <form onSubmit={handleCreateNewTask} className={styles.form} action="">
       <div className={styles.formRow}>
         <Input
           id='input'
           type='text'
           labelText='Nome do ciclo'
           placeholder='Dê um nome para o ciclo'
+          ref={taskNameInput}
         />
       </div>
 
