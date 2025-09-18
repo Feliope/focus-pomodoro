d3b99496a20c22211c95f5983d7af8eb38512457
Paulo Felipe <paulo.felipe.pfsv@gmail.com>
2025-09-18 18:47:45 -0300

    Lógica para formatar os segundos em minutos

--- src/utils/formatSecondsToMinutes.ts
+++ src/utils/formatSecondsToMinutes.ts
@@ -1,4 +1,4 @@
-export function formatedSecondsToMinut(seconds: number) {
+export function formatedSecondsToMinutes(seconds: number) {
   const minutes = String(Math.floor(seconds / 60)).padStart(2, '0');
   const secondsMod = String(Math.floor(seconds % 60)).padStart(2, '0');
 
