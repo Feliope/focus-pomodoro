9477565fb9698d7ef526f719317d8c50bf06a34d
Paulo Felipe <paulo.felipe.pfsv@gmail.com>
2025-10-29 13:42:50 -0300

    Adição de rotas para a aplicação

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,7 @@
 import { BoltIcon, HistoryIcon, HouseIcon, MoonIcon, SunIcon } from 'lucide-react';
 import { useState, useEffect } from 'react';
 import styles from './styles.module.css';
+import { Link } from 'react-router';
 
 type AvailableThemes = 'dark' | 'light';
 
@@ -27,34 +28,38 @@ export function Menu() {
 
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href="#"
+      <Link className={styles.menuLink}
+        to="/"
         aria-label='Ir para a Home'
         title='Home'
       >
         <HouseIcon />
-      </a>
+      </Link>
 
-      <a className={styles.menuLink} href="#"
+      <Link className={styles.menuLink}
+        to="#"
         aria-label='Ver Histórico'
         title='Histórico'
       >
         <HistoryIcon />
-      </a>
+      </Link>
 
-      <a className={styles.menuLink} href="#"
+      <Link className={styles.menuLink}
+        to="#"
         aria-label='Ir para as Configurações'
         title='Configurações'
       >
         <BoltIcon />
-      </a>
+      </Link>
 
-      <a className={styles.menuLink} href="#"
+      <Link className={styles.menuLink}
+        to="#"
         aria-label='Mudar Tema'
         title='Tema'
         onClick={event => handleThemeChange(event)}
       >
         { nextThemeIcon[theme] }
-      </a>
+      </Link>
     </nav>
   )
 }
