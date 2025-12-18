343eea9381b34e44857131a8f705c8a444988f27
Paulo Felipe <paulo.felipe.pfsv@gmail.com>
2025-12-18 14:46:26 -0300

    Alterações de 'a' ou 'Link' para o RouterLink

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,7 +1,7 @@
 import { BoltIcon, HistoryIcon, HouseIcon, MoonIcon, SunIcon } from 'lucide-react';
 import { useState, useEffect } from 'react';
 import styles from './styles.module.css';
-import { Link } from 'react-router';
+import { RouterLink } from '../RouterLink';
 
 type AvailableThemes = 'dark' | 'light';
 
@@ -28,38 +28,38 @@ export function Menu() {
 
   return (
     <nav className={styles.menu}>
-      <Link className={styles.menuLink}
-        to="/"
+      <RouterLink className={styles.menuLink}
+        href="/"
         aria-label='Ir para a Home'
         title='Home'
       >
         <HouseIcon />
-      </Link>
+      </RouterLink>
 
-      <Link className={styles.menuLink}
-        to="#"
+      <RouterLink className={styles.menuLink}
+        href="#"
         aria-label='Ver Histórico'
         title='Histórico'
       >
         <HistoryIcon />
-      </Link>
+      </RouterLink>
 
-      <Link className={styles.menuLink}
-        to="#"
+      <RouterLink className={styles.menuLink}
+        href="#"
         aria-label='Ir para as Configurações'
         title='Configurações'
       >
         <BoltIcon />
-      </Link>
+      </RouterLink>
 
-      <Link className={styles.menuLink}
-        to="#"
+      <RouterLink className={styles.menuLink}
+        href="#"
         aria-label='Mudar Tema'
         title='Tema'
         onClick={event => handleThemeChange(event)}
       >
         { nextThemeIcon[theme] }
-      </Link>
+      </RouterLink>
     </nav>
   )
 }
